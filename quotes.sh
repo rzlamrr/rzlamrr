6c710b177578047789effda547f962cde8364ddb
rizal <rzlamrr.dvst@protonmail.com>
2023-08-30 09:50:20 +0700

    fix

--- quotes.sh
+++ quotes.sh
@@ -56,7 +56,7 @@ main() {
 <p align=\"center\">
 <a href=\"https://rzlamrr.github.io\" alt=\"rzlamrr's top languages\"><img src=\"https://dvst-stats.vercel.app/api/top-langs/?username=rzlamrr&langs_count=6&layout=compact&theme=tokyonight&show_icons=true\"></a>
 <br>
-<a href=\"https://rzlamrr.github.io\" alt=\"rzlamrr's github streak\"><img src=\"https://dvst-streak.herokuapp.com/?user=rzlamrr&theme=tokyonight&fire=DD472C\"></a>
+<a href="https://rzlamrr.github.io" alt="rzlamrr's github streak"><img src="https://streak-stats.demolab.com?user=rzlamrr&theme=tokyonight&mode=weekly"></a>
 <br>
 <a href=\"https://rzlamrr.github.io\" alt=\"rzlamrr's github stat\"><img src=\"https://dvst-stats.vercel.app/api?username=rzlamrr&theme=tokyonight&show_icons=true\"></a>
 </p>
@@ -86,7 +86,7 @@ main() {
 <h2 align=\"center\">Thank You 🙏🏼</h2>
 <p align=\"center\"><b><i>Nothing special with me!</i></b></p>
 <p align=\"center\">
-<a href=\"https://rzlamrr.github.io\" alt=\"rzlamrr's profile visitors\"><img src=\"https://dvstr.herokuapp.com/badge\">
+<a href=\"https://rzlamrr.github.io\" alt=\"rzlamrr's profile visitors\"><img src=\"https://hits.dwyl.com/rzlamrr/rzlamrr.svg?style=flat\">
 </p>" > README.md
 }
 
